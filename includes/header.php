d99bbcbb6206d87f16d297f55a9a1db5434ebbfd
Aurelie <161795599+Bineta76@users.noreply.github.com>
2025-03-31 10:15:30 +0200

    epreuve e6

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,9 @@
     <link href="assets/css/styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <script src="
+https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js
+"></script>
 
     
 
--- planningmedecin.php
+++ planningmedecin.php
@@ -1,106 +1,20 @@
-<?php include 'includes/header.php';?>
-session_start();
-<div class="container">
-    
-            
-    <h2>
-        <center>Javascript Fullcalendar</center>
-    </h2>
-    <div class="container">
-        <div id="calendar"></div>
-    </div>
-    <br>
-</body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar/index.global.min.js'></script>
+    <script>
 
+      document.addEventListener('DOMContentLoaded', function() {
+        const calendarEl = document.getElementById('calendar')
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        })
+        calendar.render()
+      })
+
+    </script>
+  </head>
+  <body>
+    <div id='calendar'></div>
+  </body>
 </html>
-<script>
-   $(document).ready(function() {
-        $('#calendar').fullCalendar({
-        selectable: true,
-        selectHelper: true,
-        select: function ()
-        {
-            $('#myModal').modal('toggle');
-        },
-
-        header:
-        {
-            left: 'month, agendaWeek, agendaDay, list',
-            center:'title',
-            right:'prev, today , next',
-        },
-        buttonText:
-        {
-            today : 'TODAY',
-            month : 'Month',
-            week: 'Week',
-            day: 'Day',
-            list:'List',
-        },
-        events:[{
-            title : 'Mehndi',
-            start : '2023-02-01T09:00',
-            end : '2023-02-01T13:00',
-            color: 'yellow',
-            textColor: 'black',
-        },
-        {
-            title:'Haldi',
-            start:'2023-02-02T15:00',
-            end :'2023-02-02T17:00',
-            color: 'yellow',
-            textColor: 'black',
-        },
-        {
-            title:'Inscriptions',
-            start:'2024-12-02T10:00',
-            end :'2024-12-02T12:00',
-            color: 'yellow',
-            textColor: 'black',
-        },
-        {
-            title:'Mariage',
-            start:'2024-02-01T24:00',
-            end:'2024-02-02T18:00',
-            color: 'yellow',
-            textColor: 'black',
-            }],
-            dayRender:function (date,cell)
-            {
-                var newdate = $.fullCalendar.formatDate(date,'DD-MM-YYYY');
-                if(newdate=='24-05-2023')
-                {
-                    cell.css("background","yellow");
-                }
-                else if (newdate =='20-02-2023')
-                {
-                    cell.css("background",'red')
-                }
-
-            },
-        });
-     });
-</script>
-
-
-
-<div id="myModal" class="modal fade" role="dialog">
-    <div class="modal-dialog"></div>
-
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Create Event</h4>
-            </div>
-            <div class="modal-body">
-                <input type="text" class="form control"/>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-
-    <?php include 'includes/footer.php';?>
-
-</div>
